532510e0afe9c9d3424a84030cd663951d36c5e8
unknown <hdilhara@gmail.com>
2020-02-19 10:02:35 +0530

    jsp example

---
+++ SpringBoot/demoJSP/src/main/java/com/example/demoJSP/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.example.demoJSP.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String getPage(){
+
+        return "test";
+    }
+
+}
---
+++ SpringBoot/demoJSP/src/main/webapp/WEB-INF/jsp/test.jsp
@@ -0,0 +1,27 @@
+<html>
+<head>
+
+</head>
+
+<body>
+<p>Hello World!</p>
+<%
+String name="Thilina";
+%>
+
+
+
+
+<p>
+<%=name %>
+
+<%
+name+=" Dilhara";
+%>
+</p>
+
+<p>
+<%=name %>
+</p>
+</body>
+</html>
